18a40f02b5d821d9704d8ad75aa23d83c1e981c6
tom <tom@example.com>
2020-07-04 03:17:57 +0000

    addintion function is updated with 4 numbers

--- src/main/java/com/company/helloworld/Addition.java
+++ src/main/java/com/company/helloworld/Addition.java
@@ -4,9 +4,11 @@ public class Addition {
   public int num3;
 
   public Addition(int num1, int num2, int num3) {
+
     this.num1 = num1;
     this.num2 = num2;
     this.num3 = num3;
+
   }
 
   public int getSum() {
